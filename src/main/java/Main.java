907049412c39d5af93b91fdba50513b4077585df
Adham Mamdouh <mamdouhadham57@gmail.com>
2024-09-17 08:20:23 +0300

    feat: support multiple processes creation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,37 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(GUI::new);
+        if (args.length == 0) {
+            GUI gui = new GUI();
+
+            gui.startSocketListener();
+        } else {
+            startProcess(args);
+        }
+    }
+
+    private static void startProcess(String[] args) {
+        System.out.println("Starting process");
+        System.out.println("Args: " + String.join(", ", args));
+        int id = Integer.parseInt(args[0]);
+        int centralLoggerPort = Integer.parseInt(args[1]);
+        String otherProcessesStr = args[2];
+
+        ProcessApp process = new ProcessApp(id, centralLoggerPort);
+
+        List<String> otherProcessesList = List.of(otherProcessesStr.split(","));
+        System.out.println("Other processes: " + otherProcessesList);
+        System.out.println("Other processes size: " + otherProcessesList.size());
+        for (int i = 0; i < otherProcessesList.size(); i++) {
+            if (otherProcessesList.get(i).isEmpty()) {
+                continue;
+            }
+            System.out.println("Item: " + otherProcessesList.get(i));
+            String[] processInfo = otherProcessesList.get(i).split(":");
+            process.otherProcesses.add(new ProcessInfo(Integer.parseInt(processInfo[0]), Integer.parseInt(processInfo[1])));
+        }
+
+        new Thread(process::start).start();
     }
 }
 
